315a7e557854c988578f145fa9f01c7a54c2359b
gl.tomasz.wisniewski <tomasz.wisniewski@globallogic.com>
2016-08-02 22:21:06 +0200

    Initial Commit

---
+++ MasterOfHarvester/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  MasterOfHarvester
+//
+//  Created by Tomasz Wiśniewski on 02/08/16.
+//  Copyright (c) 2016 GlobalLogic. All rights reserved.
+//
+
+
+import Cocoa
+import SpriteKit
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var skView: SKView!
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        /* Pick a size for the scene */
+        if let scene = GameScene(fileNamed:"GameScene") {
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            self.skView!.presentScene(scene)
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            self.skView!.ignoresSiblingOrder = true
+            
+            self.skView!.showsFPS = true
+            self.skView!.showsNodeCount = true
+        }
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
+    }
+}
